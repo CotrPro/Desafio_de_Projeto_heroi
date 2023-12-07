16f2517712e87c899eaf88ed966f7a618a714465
Hugo <hugooslevit@gmail.com>
2023-12-07 23:09:48 +0000

    primeira resolução

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log ('Olá, eu vou começar a programar e mudar tudo o que eu consigo atualmente fazer.')
+// Desafio Classificador de nível de Herói
+let heroi = '';
+let xp = '';
+
+if (xp < 1000) { nivel = 'Ferro'; } else if (xp >= 1001 && xp <= 2000) { nivel = 'Bronze'; }
+else if (xp >= 2001 && xp <= 5000) { nivel = 'Prata'; }
+else if (xp >= 6001 && xp <= 7000) { nivel = 'Ouro'; }
+else if (xp >= 7001 && xp <= 8000) { nivel = 'Platina'; }
+else if (xp >= 8001 && xp <= 9000) { nivel = 'Ascendente'; }
+else if (xp >= 9001 && xp <= 10000) { nivel = 'Imortal'; }
+else if (xp >= 10001) { nivel = 'Radiante'; }
+
+console.log('O Herói de nome ' + heroi + 'está no nível de ' + nivel);
